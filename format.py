d12606cac4af4b535c84c2e214ec90a11e31e924
Ruben Eduardo Tejeda Roa <rbntejeda@hotmail.com>
2024-09-27 09:37:43 -0300

    first commit

---
+++ format.py
@@ -0,0 +1,5 @@
+chanchito = "feliz"
+a = 12
+b = 13
+
+print("Hola soy un chanchito {}".format(chanchito))
---
+++ intro.py
@@ -0,0 +1,5 @@
+"""My first Python program."""
+
+print("Hello world")
+print("Hello again " * 4)
+print("I like typing this.")
